802ede5a9bb891c240153ee169a5ff03a67c69a6
Aarav Samal <aaravsamal@gmail.com>
2023-12-01 11:55:23 -0500

    Auto stash before merge of "main" and "origin/main"

--- Practice/src/App.java
+++ Practice/src/App.java
@@ -3,5 +3,11 @@ public class App {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Run " + i);
         }
+
+        int x = 1;
+        while(x<=10){
+            System.out.println("Run " + x);
+            x++;
+        }
     }
 }
